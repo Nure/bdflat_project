f2a381f0d9bee577e18146c2b279bc2db9e21a1e
Nure <nalamsiddiq@gmail.com>
2020-01-02 05:28:29 +0600

    added django-heroku

--- bdapt/settings.py
+++ bdapt/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 
+import django_heroku
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -152,3 +154,6 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = 'nalamsiddiq@gmail.com'
 EMAIL_HOST_PASSWORD = 'xyx'
 EMAIL_USE_TLS = True
+
+
+django_heroku.settings(locals())
